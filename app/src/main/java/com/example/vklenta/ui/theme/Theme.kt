0ebf34f45e93e462596bc25934e86e12c1e83412
vdggrtff <xzxz9363@gmail.com>
2025-07-08 01:38:35 +0500

    ProfileCard with dark and light Theme

--- app/src/main/java/com/example/vklenta/ui/theme/Theme.kt
+++ app/src/main/java/com/example/vklenta/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.vklenta.ui.theme
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -9,18 +10,24 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Black900,
+    secondary = Black900,
+    tertiary = Black900,
+    surface = Color.Black,
+    onPrimary = Color.White,
+    onSecondary = Black500,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color.White,
+    secondary = Color.White,
+    tertiary = Color.White,
+    onPrimary = Black900,
+    onSecondary = Black500,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +44,7 @@ private val LightColorScheme = lightColorScheme(
 fun VKLentaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
